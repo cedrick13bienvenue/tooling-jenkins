8de62db759aa112bb6d8d828ffc025df97035725
bienvenue cedrick <cedrick4smith@gmail.com>
2026-06-19 15:36:18 +0200

    replace string-concatenation SQL with prepared statements throughout functions.php

--- html/functions.php
+++ html/functions.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 session_start();
 
-// connect to database
 $db = mysqli_connect(
     getenv('DB_HOST') ?: 'mysql.tooling.svc.cluster.local',
     getenv('DB_USER') ?: 'admin',
@@ -9,91 +8,70 @@ $db = mysqli_connect(
     getenv('DB_NAME') ?: 'tooling'
 );
 
-// Check connection
-// if (mysqli_connect_errno()) {
-// echo "Failed to connect to MySQL: " . mysqli_connect_error();
-// exit();
-// }
-// else{
-// echo "connected";
-// }
-
-// variable declaration
 $username = "";
 $email    = "";
-$errors   = array(); 
+$errors   = array();
 
-// call the register() function if register_btn is clicked
 if (isset($_POST['register_btn'])) {
 	register();
 }
 
-// REGISTER USER
 function register(){
-	// call these variables with the global keyword to make them available in function
 	global $db, $errors, $username, $email;
 
-	// receive all input values from the form. Call the e() function
-    // defined below to escape form values
-	$username    =  e($_POST['username']);
-	$email       =  e($_POST['email']);
-	$password_1  =  e($_POST['password_1']);
-	$password_2  =  e($_POST['password_2']);
+	$username   = trim($_POST['username']);
+	$email      = trim($_POST['email']);
+	$password_1 = trim($_POST['password_1']);
+	$password_2 = trim($_POST['password_2']);
 
-	// form validation: ensure that the form is correctly filled
-	if (empty($username)) { 
-		array_push($errors, "Username is required"); 
+	if (empty($username)) {
+		array_push($errors, "Username is required");
 	}
-	if (empty($email)) { 
-		array_push($errors, "Email is required"); 
+	if (empty($email)) {
+		array_push($errors, "Email is required");
 	}
-	if (empty($password_1)) { 
-		array_push($errors, "Password is required"); 
+	if (empty($password_1)) {
+		array_push($errors, "Password is required");
 	}
 	if ($password_1 != $password_2) {
 		array_push($errors, "The two passwords do not match");
 	}
 
-	// register user if there are no errors in the form
 	if (count($errors) == 0) {
 		$password = password_hash($password_1, PASSWORD_BCRYPT);
 
 		if (isset($_POST['user_type'])) {
-			$user_type = e($_POST['user_type']);
-			$query = "INSERT INTO users (username, email, user_type, password) 
-					  VALUES('$username', '$email', '$user_type', '$password')";
-			mysqli_query($db, $query);
-			$_SESSION['success']  = "New user successfully created!!";
+			$user_type = trim($_POST['user_type']);
+			$stmt = mysqli_prepare($db, "INSERT INTO users (username, email, user_type, password) VALUES(?, ?, ?, ?)");
+			mysqli_stmt_bind_param($stmt, "ssss", $username, $email, $user_type, $password);
+			mysqli_stmt_execute($stmt);
+			$_SESSION['success'] = "New user successfully created!!";
 			header('location: admin_tooling.php');
-		}else{
-			$query = "INSERT INTO users (username, email, user_type, password) 
-					  VALUES('$username', '$email', 'user', '$password')";
-			mysqli_query($db, $query);
+		} else {
+			$user_type = 'user';
+			$stmt = mysqli_prepare($db, "INSERT INTO users (username, email, user_type, password) VALUES(?, ?, ?, ?)");
+			mysqli_stmt_bind_param($stmt, "ssss", $username, $email, $user_type, $password);
+			mysqli_stmt_execute($stmt);
 
-			// get id of the created user
 			$logged_in_user_id = mysqli_insert_id($db);
-
-			$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
-			$_SESSION['success']  = "You are now logged in";
-			header('location: index.php');				
+			$_SESSION['user'] = getUserById($logged_in_user_id);
+			$_SESSION['success'] = "You are now logged in";
+			header('location: index.php');
 		}
 	}
 }
 
-// return user array from their id
 function getUserById($id){
 	global $db;
-	$query = "SELECT * FROM users WHERE id=" . $id;
-	$result = mysqli_query($db, $query);
-
-	$user = mysqli_fetch_assoc($result);
-	return $user;
+	$stmt = mysqli_prepare($db, "SELECT * FROM users WHERE id=?");
+	mysqli_stmt_bind_param($stmt, "i", $id);
+	mysqli_stmt_execute($stmt);
+	$result = mysqli_stmt_get_result($stmt);
+	return mysqli_fetch_assoc($result);
 }
 
-// escape string
 function e($val){
-	global $db;
-	return mysqli_real_escape_string($db, trim($val));
+	return trim($val);
 }
 
 function display_error() {
@@ -102,7 +80,7 @@ function display_error() {
 	if (count($errors) > 0){
 		echo '<div class="error">';
 			foreach ($errors as $error){
-				echo $error .'<br>';
+				echo htmlspecialchars($error) . '<br>';
 			}
 		echo '</div>';
 	}
@@ -110,11 +88,7 @@ function display_error() {
 
 function isLoggedIn()
 {
-	if (isset($_SESSION['user'])) {
-		return true;
-	}else{
-		return false;
-	}
+	return isset($_SESSION['user']);
 }
 
 if (isset($_GET['logout'])) {
@@ -123,20 +97,16 @@ if (isset($_GET['logout'])) {
 	header("location: login.php");
 }
 
-// call the login() function if register_btn is clicked
 if (isset($_POST['login_btn'])) {
 	login();
 }
 
-// LOGIN USER
 function login(){
 	global $db, $username, $errors;
 
-	// grap form values
-	$username = e($_POST['username']);
-	$password = e($_POST['password']);
+	$username = trim($_POST['username']);
+	$password = trim($_POST['password']);
 
-	// make sure form is filled properly
 	if (empty($username)) {
 		array_push($errors, "Username is required");
 	}
@@ -144,16 +114,17 @@ function login(){
 		array_push($errors, "Password is required");
 	}
 
-	// attempt login if no errors on form
 	if (count($errors) == 0) {
-		$query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
-		$results = mysqli_query($db, $query);
+		$stmt = mysqli_prepare($db, "SELECT * FROM users WHERE username=? LIMIT 1");
+		mysqli_stmt_bind_param($stmt, "s", $username);
+		mysqli_stmt_execute($stmt);
+		$results = mysqli_stmt_get_result($stmt);
 
 		if (mysqli_num_rows($results) == 1) {
 			$logged_in_user = mysqli_fetch_assoc($results);
 			if (password_verify($password, $logged_in_user['password'])) {
 				$_SESSION['user'] = $logged_in_user;
-				$_SESSION['success']  = "You are now logged in";
+				$_SESSION['success'] = "You are now logged in";
 				if ($logged_in_user['user_type'] == 'admin') {
 					header('location: admin_tooling.php');
 				} else {
@@ -170,11 +141,5 @@ function login(){
 
 function isAdmin()
 {
-	if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin' ) {
-		return true;
-	}else{
-		return false;
-	}
+	return isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin';
 }
-
-
